87fbc4219d24ba7153c733872c6af70934674710
Valerio Vaudi <valerio.vaudi@gmail.com>
2014-07-26 20:30:27 +0200

    security config update

--- src/main/java/it/valeriovaudi/factory/SecurityUserFactoryImpl.java
+++ src/main/java/it/valeriovaudi/factory/SecurityUserFactoryImpl.java
@@ -1,12 +1,14 @@
 package it.valeriovaudi.factory;
 
 import it.valeriovaudi.web.model.UserDTO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.Collections;
 
@@ -14,18 +16,25 @@ import java.util.Collections;
  * Created by Valerio on 26/07/2014.
  */
 public class SecurityUserFactoryImpl implements SecurityUserFactory {
+
+    private PasswordEncoder passwordEncoder;
+
     @Override
     public Authentication getAutenticatedUser(UserDTO userDTO) {
-        return new UsernamePasswordAuthenticationToken(createUser(userDTO), userDTO.getPassword(), Collections.singleton(createAuthority()));
+        return new UsernamePasswordAuthenticationToken(createUser(userDTO), passwordEncoder.encode(userDTO.getPassword()) , Collections.singleton(createAuthority()));
     }
 
     @Override
     public UserDetails createUser(UserDTO userDTO) {
-        return new User(userDTO.getUserName(), userDTO.getPassword(), Collections.singleton(createAuthority()));
-    }
+        return new User(userDTO.getUserName(), passwordEncoder.encode(userDTO.getPassword()), Collections.singleton(createAuthority()));
+}
 
     private GrantedAuthority createAuthority() {
         return new SimpleGrantedAuthority("ROLE_ADMIN");
     }
 
+    @Autowired
+    public void setPasswordEncoder(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
 }

9ef7c5d0743f999a3b73c22436f57d66411d3aff
Valerio Vaudi <valerio.vaudi@gmail.com>
2014-09-20 14:59:56 +0200

    test failure fix

--- src/test/java/it/valeriovaudi/controller/IndexControllerTest.java
+++ src/test/java/it/valeriovaudi/controller/IndexControllerTest.java
@@ -1,6 +1,7 @@
 package it.valeriovaudi.controller;
 
 import org.junit.Test;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -8,13 +9,18 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 /**
  * Created by Valerio on 26/07/2014.
  */
-public class IndexControllerTest extends AbstractTest {
+public class IndexControllerTest extends AbstractTestWithSecurityContext {
 
     @Test
     public void getAllpersoneTest() throws Exception {
-        mockMvc.perform(get("/index")).
+        mockMvc.perform(get("/index").
+                sessionAttr(HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY,principal)).
                 andExpect(status().isOk()).
                 andReturn();
     }
 
+    @Override
+    protected Object principalInit() throws Exception {
+        return authenticate();
+    }
 }

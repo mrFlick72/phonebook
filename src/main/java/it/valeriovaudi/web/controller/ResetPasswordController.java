4e014b5231a10b3efa05cbda80e116f14a06b31c
Valerio Vaudi <valerio.vaudi@gmail.com>
2014-10-25 01:28:52 +0200

    reset password

--- src/main/java/it/valeriovaudi/web/controller/ResetPasswordController.java
+++ src/main/java/it/valeriovaudi/web/controller/ResetPasswordController.java
@@ -21,12 +21,15 @@ public class ResetPasswordController {
         this.passwordService = passwordService;
     }
 
+    // todo insert the logic for reject ger request for nonce that are used or expired
     @RequestMapping(value = "/resetPassword/reset", method = RequestMethod.GET)
     public void resetPasswordInit(@RequestParam(value = "nonce") String nonce,Model model) {
+
         model.addAttribute("nonce", nonce);
         model.addAttribute("controller", "resetPasswordController");
     }
 
+    // todo insert the logic in the pipe line resetPassword for reject ger request for nonce that are used or expired
     @RequestMapping(value = "/resetPassword/reset", method = RequestMethod.POST)
     public void resetPassword(@ModelAttribute(value = "nonce") String nonce,
                               @RequestParam(value = "newPassword") String newPassword) {

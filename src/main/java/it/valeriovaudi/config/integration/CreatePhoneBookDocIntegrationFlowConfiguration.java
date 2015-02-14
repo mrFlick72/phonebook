089301868f4a5140d382ddf42abe1eeca3bca76d
Valerio Vaudi <valerio.vaudi@gmail.com>
2015-02-14 23:29:06 +0100

    bean profiling: implementation of phoneBookDoc in traditional xml and in java-dsl through bean profiling

--- src/main/java/it/valeriovaudi/config/integration/CreatePhoneBookDocIntegrationFlowConfiguration.java
+++ src/main/java/it/valeriovaudi/config/integration/CreatePhoneBookDocIntegrationFlowConfiguration.java
@@ -1,7 +1,6 @@
 package it.valeriovaudi.config.integration;
 
 import it.valeriovaudi.repository.ContactRepository;
-import it.valeriovaudi.web.model.Contact;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
@@ -10,35 +9,71 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.integration.config.EnableIntegration;
-import org.springframework.integration.dsl.GenericEndpointSpec;
 import org.springframework.integration.dsl.IntegrationFlow;
-import org.springframework.integration.dsl.RouterSpec;
-import org.springframework.integration.dsl.core.MessageHandlerSpec;
 import org.springframework.integration.dsl.mail.Mail;
-import org.springframework.integration.dsl.support.Consumer;
-import org.springframework.integration.dsl.support.GenericHandler;
 import org.springframework.integration.jms.ChannelPublishingJmsMessageListener;
 import org.springframework.integration.jms.JmsMessageDrivenEndpoint;
-import org.springframework.integration.transformer.GenericTransformer;
 import org.springframework.jms.listener.DefaultMessageListenerContainer;
 import org.springframework.jms.support.destination.DynamicDestinationResolver;
-import org.springframework.messaging.Message;
-import org.springframework.messaging.MessageHandler;
-import org.springframework.messaging.MessagingException;
 
 import javax.jms.ConnectionFactory;
 import javax.jms.Destination;
-import java.util.Map;
 
 /**
  * Created by Valerio on 14/02/2015.
  */
 
-/*
-*  <int-jms:message-driven-channel-adapter
-            destination="sendQueue"
-            channel="boockPrintServiceChain"/>
+@Configuration
+@EnableIntegration
+@Profile(value = "java-config")
+@PropertySource(value = "classpath:mail.properties")
+public class CreatePhoneBookDocIntegrationFlowConfiguration {
 
+    @Autowired
+    private Environment environment;
+
+    @Autowired
+    private ContactRepository contactRepository;
+
+    @Autowired
+    private ConnectionFactory connectionFactory;
+
+    @Autowired
+    @Qualifier("sendQueue")
+    private Destination destination;
+
+    /*
+    <int-jms:message-driven-channel-adapter
+        destination="sendQueue"
+        channel="boockPrintServiceChain"/>
+
+        equivalent Java Config configuration of namespace int-jms:message-driven-channel-adapter
+     */
+    @Bean
+    public DefaultMessageListenerContainer defaultMessageListenerContainer(){
+        DefaultMessageListenerContainer defaultMessageListenerContainer = new DefaultMessageListenerContainer();
+        defaultMessageListenerContainer.setDestination(destination);
+        defaultMessageListenerContainer.setConnectionFactory(connectionFactory);
+
+        return defaultMessageListenerContainer;
+    }
+
+    @Bean
+    public ChannelPublishingJmsMessageListener messageDrivenChannelAdapter4CreatePhoneBookDocIntegrationFlow(){
+        ChannelPublishingJmsMessageListener channelPublishingJmsMessageListener = new ChannelPublishingJmsMessageListener();
+
+        channelPublishingJmsMessageListener.setDestinationResolver(new DynamicDestinationResolver());
+        channelPublishingJmsMessageListener.setRequestChannelName("boockPrintServiceChain");
+        return channelPublishingJmsMessageListener;
+    }
+
+    @Bean
+    public JmsMessageDrivenEndpoint jmsMessageDrivenEndpoint4CreatePhoneBookDocIntegrationFlow(){
+        JmsMessageDrivenEndpoint jmsMessageDrivenEndpoint = new JmsMessageDrivenEndpoint(defaultMessageListenerContainer(),messageDrivenChannelAdapter4CreatePhoneBookDocIntegrationFlow());
+        return jmsMessageDrivenEndpoint;
+    }
+
+/*
     <chain input-channel="boockPrintServiceChain"
            output-channel="boockPrintServiceDocumentTypeRouter">
         <header-enricher>
@@ -73,78 +108,33 @@ import java.util.Map;
         <transformer ref="mailMessageTrasformer"/>
         <mail:outbound-channel-adapter mail-sender="mailSender" />
     </chain>
+
+
+    equivalent java-dsl configuration of the Spring Integration pipeline
+    in this exapmle i have used the java 8 capability with lambada expressions
     */
-@Configuration
-@EnableIntegration
-@Profile(value = "java-config")
-@PropertySource(value = "classpath:mail.properties")
-public class CreatePhoneBookDocIntegrationFlowConfiguration {
-
-    /*
-    *  <int-jms:message-driven-channel-adapter
-            destination="sendQueue"
-            channel="boockPrintServiceChain"/>
-            */
-
-    @Autowired
-    private Environment environment;
-
-    @Autowired
-    private ContactRepository contactRepository;
-
-    @Autowired
-    private ConnectionFactory connectionFactory;
-
-    @Autowired
-    @Qualifier("sendQueue")
-    private Destination destination;
 
     @Bean
-    public DefaultMessageListenerContainer defaultMessageListenerContainer(){
-        DefaultMessageListenerContainer defaultMessageListenerContainer = new DefaultMessageListenerContainer();
-        defaultMessageListenerContainer.setDestination(destination);
-        defaultMessageListenerContainer.setConnectionFactory(connectionFactory);
-
-        return defaultMessageListenerContainer;
-    }
-
-    @Bean
-    public ChannelPublishingJmsMessageListener messageDrivenChannelAdapter4CreatePhoneBookDocIntegrationFlow(){
-        ChannelPublishingJmsMessageListener channelPublishingJmsMessageListener = new ChannelPublishingJmsMessageListener();
-
-        channelPublishingJmsMessageListener.setDestinationResolver(new DynamicDestinationResolver());
-        channelPublishingJmsMessageListener.setRequestChannelName("boockPrintServiceChain");
-        return channelPublishingJmsMessageListener;
-    }
-
-    @Bean
-    public JmsMessageDrivenEndpoint jmsMessageDrivenEndpoint4CreatePhoneBookDocIntegrationFlow(){
-        JmsMessageDrivenEndpoint jmsMessageDrivenEndpoint = new JmsMessageDrivenEndpoint(defaultMessageListenerContainer(),messageDrivenChannelAdapter4CreatePhoneBookDocIntegrationFlow());
-        return jmsMessageDrivenEndpoint;
-    }
-
-
-
-    @Bean
-    public IntegrationFlow createPhoneBookDocIntegrationFlow(){
-        return f -> {f.channel("boockPrintServiceChain")
-                .enrichHeaders(headerEnricherSpec -> {
-                    headerEnricherSpec.headerExpression("user", "@phonBookUserRepository.findByUserName(payload['userName'])");
-                    headerEnricherSpec.headerExpression("contentType", "payload['contentType']");
-                })
-                .transform("payload['userName']")
-                .handle((payload, headers) -> contactRepository.findAllContactByUser((String) payload).iterator())
-            .transform("T(org.apache.commons.collections.IteratorUtils).toList(payload)")
-                .route("headers['contentType']", stringRouterSpec -> {
-                    stringRouterSpec
-                            .subFlowMapping("pdf", subFlow -> subFlow.handle("pdfCreatePhoneBoockDoc", "createPhoneBoockDoc"))
-                            .subFlowMapping("docx", subFlow -> subFlow.handle("wordCreatePhoneBoockDoc", "createPhoneBoockDoc"));
-                })
-                .transform("@mailMessageTrasformer.trasformMessage(headers['contentType'],headers['user'],payload)")
-                .enrichHeaders(Mail.headers()
-                        .toExpression("headers['user'].mail")
-                        .from(environment.getProperty("mail.admin"))
-                        .subject(environment.getProperty("mail.createphoneBookDocument.subject")))
+    public IntegrationFlow createPhoneBookDocIntegrationFlow() {
+        return f -> {
+            f.channel("boockPrintServiceChain")
+                    .enrichHeaders(headerEnricherSpec -> {
+                        headerEnricherSpec.headerExpression("user", "@phonBookUserRepository.findByUserName(payload['userName'])");
+                        headerEnricherSpec.headerExpression("contentType", "payload['contentType']");
+                    })
+                    .transform("payload['userName']")
+                    .handle((payload, headers) -> contactRepository.findAllContactByUser((String) payload).iterator())
+                    .transform("T(org.apache.commons.collections.IteratorUtils).toList(payload)")
+                    .route("headers['contentType']", stringRouterSpec -> {
+                        stringRouterSpec
+                                .subFlowMapping("pdf", subFlow -> subFlow.handle("pdfCreatePhoneBoockDoc", "createPhoneBoockDoc"))
+                                .subFlowMapping("docx", subFlow -> subFlow.handle("wordCreatePhoneBoockDoc", "createPhoneBoockDoc"));
+                    })
+                    .transform("@mailMessageTrasformer.trasformMessage(headers['contentType'],headers['user'],payload)")
+                    .enrichHeaders(Mail.headers()
+                            .toExpression("headers['user'].mail")
+                            .from(environment.getProperty("mail.admin"))
+                            .subject(environment.getProperty("mail.createphoneBookDocument.subject")))
                 .handle(Mail.outboundAdapter("localhost"));
         };
     }

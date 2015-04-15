5e1c3e9a15d35cbda6ab6b98b7530ba1e125fc25
Valerio Vaudi <valerio.vaudi@gmail.com>
2015-04-15 21:25:10 +0200

    better usage of lambda

--- src/main/java/it/valeriovaudi/web/controller/IndexPageController.java
+++ src/main/java/it/valeriovaudi/web/controller/IndexPageController.java
@@ -10,6 +10,9 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by Valerio on 19/09/2014.
  */
@@ -30,23 +33,18 @@ public class IndexPageController {
     @Secured(value = "IS_AUTHENTICATED_FULLY")
     @RequestMapping(value = "/index",method = RequestMethod.GET)
     public void getUserPage(Model model){
+        final Map<PhoneBookSecurityRole,String> role2COntroller = new HashMap<>();
+        role2COntroller.put(PhoneBookSecurityRole.ADMIN,"administrationController");
+        role2COntroller.put(PhoneBookSecurityRole.USER,"handleFormController");
+
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         model.addAttribute("phoneBoockUserName", authentication.getName());
         String jsController =  authentication.getAuthorities().
                 stream().
-                map(grantedAuthority -> {
-                    String controllerName = "";
-                    if (grantedAuthority.getAuthority().
-                            equals(PhoneBookSecurityRole.ADMIN.getRole())) {
-                        controllerName = "administrationController";
-                    } else if (grantedAuthority.getAuthority().
-                            equals(PhoneBookSecurityRole.USER.getRole())) {
-                        controllerName = "handleFormController";
-                    }
-                    return controllerName;
-                }).
+                map(grantedAuthority -> role2COntroller.get(grantedAuthority.getAuthority())).
                 findFirst().
                 get();
         model.addAttribute("controller", jsController);
     }
 }
+
